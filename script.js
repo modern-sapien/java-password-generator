4dc9e31057caa660951fc5dbf8bb1427f2cb8458
Jonathan Canales <joncanales2@gmail.com>
2020-09-02 23:03:55 -0400

    beginning pseudo code & establishing object & variables

--- script.js
+++ script.js
@@ -7,12 +7,39 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+
 function generatePassword() {
   //TODO generate a real password & replace return string with real password string
   return "This is not my final password!"
 }
 
+  var passCondition {                             //create an object with multiple arrays that we will call on to generate a password depending on preferences
+        var passLowerCase = ["a", "b", "c", "d", "e", "f"];
+        var passUpperCase = ["A", "B", "C", "D", "E", "F"];
+        var passNumber = ["1", "2", "3", "4", "5", "6"];
+        var passSpecial = ["!", "@", "#", "%", "^"];
+        }
+
+
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// GIVEN I need a new, secure password
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+// WHEN prompted for character types to include in the password
+// THEN I choose lowercase, uppercase, numeric, and/or special characters
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
